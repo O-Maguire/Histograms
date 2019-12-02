3f0f3f5e076cd34fb205c09ad31e00e11c02f033
Oisin Maguire <39838528+O-Maguire@users.noreply.github.com>
2019-12-02 13:23:34 +0100

    Updates to Binning.m
    Fixed issue with only allowing 10 bins to be created.

--- Binning.m
+++ Binning.m
@@ -1,4 +1,6 @@
-function Binning(data,Number_of_bins,Xlabel,Ylabel,DecimalPlaces)
+%Version 1.1
+%Fixed issue with only allowing 10 bins
+function [Values,Edges]=Binning(data,Number_of_bins,Xlabel,Ylabel,DecimalPlaces)
 format shortg
 
 Mindata=min(data);
@@ -15,20 +17,13 @@ for ii=1:Number_of_bins
 end
 
 [x]=histogram(data,Number_of_bins)%,'BinEdges',Bins)
-string1=sprintf("%s to %s",num2str(round(x.BinEdges(1),DecimalPlaces)),num2str(round(x.BinEdges(2),DecimalPlaces)));
-string2=sprintf("%s to %s",num2str(round(x.BinEdges(2),DecimalPlaces)),num2str(round(x.BinEdges(3),DecimalPlaces)));
-string3=sprintf("%s to %s",num2str(round(x.BinEdges(3),DecimalPlaces)),num2str(round(x.BinEdges(4),DecimalPlaces)));
-string4=sprintf("%s to %s",num2str(round(x.BinEdges(4),DecimalPlaces)),num2str(round(x.BinEdges(5),DecimalPlaces)));
-string5=sprintf("%s to %s",num2str(round(x.BinEdges(5),DecimalPlaces)),num2str(round(x.BinEdges(6),DecimalPlaces)));
-string6=sprintf("%s to %s",num2str(round(x.BinEdges(6),DecimalPlaces)),num2str(round(x.BinEdges(7),DecimalPlaces)));
-string7=sprintf("%s to %s",num2str(round(x.BinEdges(7),DecimalPlaces)),num2str(round(x.BinEdges(8),DecimalPlaces)));
-string8=sprintf("%s to %s",num2str(round(x.BinEdges(8),DecimalPlaces)),num2str(round(x.BinEdges(9),DecimalPlaces)));
-string9=sprintf("%s to %s",num2str(round(x.BinEdges(9),DecimalPlaces)),num2str(round(x.BinEdges(10),DecimalPlaces)));
-string10=sprintf("%s to %s",num2str(round(x.BinEdges(10),DecimalPlaces)),num2str(round(x.BinEdges(11),DecimalPlaces)));
-
+strings={};
+for ii=1:Number_of_bins
+    strings{end+1}=sprintf("%s to %s",num2str(round(x.BinEdges(ii),DecimalPlaces)),num2str(round(x.BinEdges(ii+1),DecimalPlaces)));
+end
 
 dif=(x.BinEdges(2)-x.BinEdges(1))./2;
-set(gca,'XTick',[x.BinEdges(1)+dif:x.BinEdges(2)-x.BinEdges(1):x.BinEdges(11)+dif],'XTickLabel',{string1;string2;string3;string4;string5;string6;string7;string8;string9;string10;})
+set(gca,'XTick',[x.BinEdges(1)+dif:x.BinEdges(2)-x.BinEdges(1):x.BinEdges(Number_of_bins+1)+dif],'XTickLabel',(strings))
 xtickangle(30)
 if nargin<=2
     xlabel('Bin edge positions')
@@ -37,5 +32,7 @@ else
     xlabel(sprintf('%s',Xlabel))
     ylabel(sprintf('%s',Ylabel))
 end
-xlim([x.BinEdges(1)-dif./3 x.BinEdges(11)+dif./3])
+xlim([x.BinEdges(1)-dif./3 x.BinEdges(Number_of_bins+1)+dif./3])
+Values(:,1)=x.Values(:);
+Edges(:,1)=x.BinEdges(:);
 end
